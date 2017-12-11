1cc90c0961809597b617ae3e97abcc5867537a35
Nasser X <nao1422@gmail.com>
2017-12-11 17:11:59 +0300

    Front-End: Update main pages layout.

--- EndToEnd/Controllers/HomeController.cs
+++ EndToEnd/Controllers/HomeController.cs
@@ -13,17 +13,13 @@ namespace EndToEnd.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult News()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
